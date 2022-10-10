5cdf9fe4c9d5f8b437abeca288f8d171d8a045df
kimtaekang <k_2684@naver.com>
2022-10-10 14:56:47 +0900

    Feat : 학식 데이터 가져오기

--- MyongJunSickFE/App.js
+++ MyongJunSickFE/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   ScrollView,
   View,
@@ -8,161 +8,178 @@ import {
   Dimensions,
   TouchableOpacity,
 } from 'react-native';
+import Config from 'react-native-config';
+import axios from 'axios';
 
-const {width: SCREEN_WIDTH} = Dimensions.get('window');
-
-const onPress = () => {
-  console.log('onPress');
-};
-
-const Header = () => {
-  return (
-    <View style={{flex: 1, flexDirection: 'row', marginLeft: 15, marginTop: 54}}>
-      <Image
-        style={{width: 32, height: 32, marginTop: 12}}
-        source={require('./assets/CalendarIcon.png')}
-      />
-      <Text
-        style={{
-          width: 68,
-          height: 41,
-          fontSize: 28,
-          fontWeight: 'bold',
-          fontStyle: 'normal',
-          letterSpacing: 0,
-          textAlign: 'center',
-          color: '#071648',
-          marginLeft: 10,
-          marginTop: 12,
-        }}
-      >
-        June
-      </Text>
-      <Text
-        style={{
-          width: 37,
-          height: 23,
-          fontSize: 16,
-          fontWeight: '500',
-          fontStyle: 'normal',
-          letterSpacing: 0,
-          textAlign: 'center',
-          color: '#a8a8a8',
-          marginLeft: 10,
-          marginTop: 20,
-        }}
-      >
-        2022
-      </Text>
-    </View>
-  );
-};
-
-const WeekCarousel = () => {
-  return (
-    <View style={{flex: 10, flexDirection: 'row', marginHorizontal: 44, marginTop: 20}}>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
-          <View
-            style={{
-              width: 44,
-              height: 64,
-              borderRadius: 8,
-              backgroundColor: '#071648',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={dayStyle}>S</Text>
-            <Text style={weekStyle}>1</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>M</Text>
-        <Text style={weekStyle}>2</Text>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>T</Text>
-        <Text style={weekStyle}>3</Text>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>W</Text>
-        <Text style={weekStyle}>4</Text>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>T</Text>
-        <Text style={weekStyle}>5</Text>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>F</Text>
-        <Text style={weekStyle}>6</Text>
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={dayStyle}>S</Text>
-        <Text style={weekStyle}>7</Text>
-      </View>
-    </View>
-  );
-};
-
-const meal = {
-  lunch: '오늘의 중식',
-  lunchTime: '11:30 - 14:00',
-  dinner: '오늘의 석식',
-  dinnerTime: '17:30 - 19:00',
-};
-
-const MealTitle = props => {
-  return (
-    <View style={mealTitle.component}>
-      <Text style={mealTitle.title}>{props.type}</Text>
-      <Text style={mealTitle.time}>{props.time}</Text>
-    </View>
-  );
-};
-
-const dummyData = {
-  lunch: ['꼬치어묵우동', '후리가케밥', '가라아게&갈릭마요소스', '단무지', '배추김치'],
-  dinner: ['일본식 카레라이스', '우동국물', '왕새우튀김', '오이지무침', '배추김치'],
-};
-
-const MenuList = props => {
-  const data = props.data; //lunch or dinner
-  return (
-    <View style={menuComponent}>
-      {data.map(menu => (
-        <ListItems key={menu} menu={menu} />
-      ))}
-    </View>
-  );
-};
-
-const ListItems = props => {
-  return (
-    <View style={item.component}>
-      <Text style={item.title}>{props.menu}</Text>
-      <Text style={item.kcal}>칼로리가 없네?</Text>
-    </View>
-  );
-};
-
-const Btn = props => {
-  return (
-    <View style={btn.default}>
-      <Text style={btn.text}>{props.btnName}</Text>
-    </View>
-  );
-};
-
-const MealSatisfaction = props => {
-  return (
-    <View style={mealSatisfaction.component}>
-      <Text style={mealSatisfaction.text}>{props.message}</Text>
-    </View>
-  );
-};
-
+Config.API_URL;
 const App = () => {
+  const {width: SCREEN_WIDTH} = Dimensions.get('window');
+
+  const onPress = () => {
+    console.log('onPress');
+  };
+
+  const Header = () => {
+    return (
+      <View style={{flex: 1, flexDirection: 'row', marginLeft: 15, marginTop: 54}}>
+        <Image
+          style={{width: 32, height: 32, marginTop: 12}}
+          source={require('./assets/CalendarIcon.png')}
+        />
+        <Text
+          style={{
+            width: 68,
+            height: 41,
+            fontSize: 28,
+            fontWeight: 'bold',
+            fontStyle: 'normal',
+            letterSpacing: 0,
+            textAlign: 'center',
+            color: '#071648',
+            marginLeft: 10,
+            marginTop: 12,
+          }}
+        >
+          June
+        </Text>
+        <Text
+          style={{
+            width: 37,
+            height: 23,
+            fontSize: 16,
+            fontWeight: '500',
+            fontStyle: 'normal',
+            letterSpacing: 0,
+            textAlign: 'center',
+            color: '#a8a8a8',
+            marginLeft: 10,
+            marginTop: 20,
+          }}
+        >
+          2022
+        </Text>
+      </View>
+    );
+  };
+
+  const WeekCarousel = () => {
+    return (
+      <View style={{flex: 10, flexDirection: 'row', marginHorizontal: 44, marginTop: 20}}>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            <View
+              style={{
+                width: 44,
+                height: 64,
+                borderRadius: 8,
+                backgroundColor: '#071648',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={dayStyle}>S</Text>
+              <Text style={weekStyle}>1</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>M</Text>
+          <Text style={weekStyle}>2</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>T</Text>
+          <Text style={weekStyle}>3</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>W</Text>
+          <Text style={weekStyle}>4</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>T</Text>
+          <Text style={weekStyle}>5</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>F</Text>
+          <Text style={weekStyle}>6</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={dayStyle}>S</Text>
+          <Text style={weekStyle}>7</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const meal = {
+    lunch: '오늘의 중식',
+    lunchTime: '11:30 - 14:00',
+    dinner: '오늘의 석식',
+    dinnerTime: '17:30 - 19:00',
+  };
+
+  const MealTitle = props => {
+    return (
+      <View style={mealTitle.component}>
+        <Text style={mealTitle.title}>{props.type}</Text>
+        <Text style={mealTitle.time}>{props.time}</Text>
+      </View>
+    );
+  };
+
+  const dummyData = {
+    lunch: ['꼬치어묵우동', '후리가케밥', '가라아게&갈릭마요소스', '단무지', '배추김치'],
+    dinner: ['일본식 카레라이스', '우동국물', '왕새우튀김', '오이지무침', '배추김치'],
+  };
+
+  const MenuList = props => {
+    const data = props.data; //lunch or dinner
+    return (
+      <View style={menuComponent}>
+        {data.map(menu => (
+          <ListItems key={menu} menu={menu} />
+        ))}
+      </View>
+    );
+  };
+
+  const ListItems = props => {
+    return (
+      <View style={item.component}>
+        <Text style={item.title}>{props.menu}</Text>
+        <Text style={item.kcal}>칼로리가 없네?</Text>
+      </View>
+    );
+  };
+
+  const Btn = props => {
+    return (
+      <View style={btn.default}>
+        <Text style={btn.text}>{props.btnName}</Text>
+      </View>
+    );
+  };
+
+  const MealSatisfaction = props => {
+    return (
+      <View style={mealSatisfaction.component}>
+        <Text style={mealSatisfaction.text}>{props.message}</Text>
+      </View>
+    );
+  };
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const result = await axios.get(`${Config.API_URL}/info`);
+      setData(result.data);
+      console.log(result.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
       <Header />
