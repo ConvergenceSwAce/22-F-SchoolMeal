0431ad91d325e3ec19391b05a1bf5144b474c2ff
kimtaekang <k_2684@naver.com>
2022-10-30 23:59:11 +0900

    Feat : 모달만 만들어 놓음

--- MyongJunSickFE/components/SettingModal.js
+++ MyongJunSickFE/components/SettingModal.js
@@ -1,9 +1,55 @@
-import React, {useState} from 'react';
-import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Modal, StyleSheet, Text, Pressable, View, Switch} from 'react-native';
 import {fontPercentage, heightPercentage, widthPercentage} from '../Responsive';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const SettingModal = () => {
+  const [selectData, setSelectData] = useState('인문캠퍼스');
+
+  useEffect(() => {
+    setSelectData(get());
+    selectData === 'seoul' ? setIsEnabled(true) : setIsEnabled(false); // AsyncStorage에 저장된 데이터가 'seoul'이면 true, 'yongin'이면 false를 반환한다. (Switch의 값이 바뀐다.)
+  }, []); // AsyncStorage에서 데이터를 가져와서 selectData에 저장한다.
+
+  const clearAll = async () => {
+    try {
+      await AsyncStorage.clear();
+    } catch (e) {
+      // clear error
+    }
+  };
+
+  const save = async () => {
+    try {
+      const select = isEnabled ? 'seoul' : 'yongin';
+      await AsyncStorage.setItem('campus', select, () => {
+        clearAll();}
+        );
+      AsyncStorage.getItem().then(value => console.log(JSON.stringify(value))); // AsyncStorage에 저장된 데이터를 출력한다. (이 때, AsyncStorage에 저장된 데이터가 출력된다.)
+    } catch (e) {
+      // 오류 예외 처리
+    }
+  };
+
+  const key = 'campus';
+  const get = async () => {
+    try {
+      const rawData = await AsyncStorage.getItem(key); // AsyncStorage에서 데이터를 가져온다.
+      if (!rawData) {
+        throw new Error('No saved ' + key);
+      }
+      const savedData = rawData;
+      return savedData;
+    } catch (e) {
+      throw new Error('Failed to load ' + key);
+    }
+  };
+
   const [modalVisible, setModalVisible] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -18,12 +64,24 @@ const SettingModal = () => {
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text style={styles.modalTitle}>환경설정</Text>
-
+            <Text style={styles.modalText}>초기화면 설정</Text>
+            <Switch
+              trackColor={{false: '#767577', true: '#005eb8'}}
+              thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+              ios_backgroundColor="#767577"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            <Text style={styles.modalText}>{isEnabled ? '자연캠퍼스' : '인문캠퍼스'}</Text>
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                clearAll();
+                save();
+              }}
             >
-              <Text style={styles.textStyle}>Hide Modal</Text>
+              <Text style={styles.textStyle}>적용하기</Text>
             </Pressable>
           </View>
         </View>
@@ -64,7 +122,8 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   buttonClose: {
-    backgroundColor: '#2196F3',
+    marginTop: heightPercentage(20),
+    backgroundColor: '#005eb8',
   },
   textStyle: {
     paddingVertical: heightPercentage(10),
@@ -74,6 +133,7 @@ const styles = StyleSheet.create({
     color: '#ffffff',
   },
   modalText: {
+    marginTop: heightPercentage(10),
     marginBottom: heightPercentage(15),
     textAlign: 'center',
   },
@@ -82,7 +142,7 @@ const styles = StyleSheet.create({
     fontSize: fontPercentage(16),
     fontFamily: 'NotoSansKR-Medium',
     color: '#071648',
-    marginBottom: heightPercentage(10),
+    marginBottom: heightPercentage(20),
   },
 });
 
