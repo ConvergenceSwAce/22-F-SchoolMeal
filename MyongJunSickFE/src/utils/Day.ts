2cb6fffa8782a1dc5dd3b0fc8ed6655331975b9d
김태강 <k_2684@naver.com>
2023-04-04 04:56:43 +0900

    feat: dayjs 일주일 날짜 get 구현

--- MyongJunSickFE/src/utils/Day.ts
+++ MyongJunSickFE/src/utils/Day.ts
@@ -1,9 +1,28 @@
 import dayjs from 'dayjs';
 
+dayjs.locale('ko');
+
 export const nowYear = (): string => {
   return dayjs().format('YYYY');
 };
 
 export const nowMonth = (): string => {
   return dayjs().format('MM');
+};
+
+export const nowDay = (): string => {
+  return dayjs().format('DD');
+};
+
+// day.js를 활용해 이번주 시작 월요일 ~ 금요일까지의 날짜를 배열로 반환
+export const getWeek = (): string[] => {
+  const week: string[] = [];
+  const today = dayjs();
+  const start = today.startOf('week');
+
+  for (let i = 1; i < 6; i++) {
+    week.push(start.add(i, 'day').format('DD'));
+  }
+
+  return week;
 };
