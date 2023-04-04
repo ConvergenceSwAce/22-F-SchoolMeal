b758720c3f646b3eb2ae2738590a99aabe338f45
김태강 <k_2684@naver.com>
2023-04-04 16:52:19 +0900

    chore: View -> Stack 컴포넌트 수정

--- MyongJunSickFE/src/components/Header.tsx
+++ MyongJunSickFE/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import {Text, View} from 'native-base';
+import {Stack, Text, View} from 'native-base';
 import React from 'react';
 import {nowMonth, nowYear} from '../utils/Day';
 
@@ -8,13 +8,13 @@ const year: string = nowYear() || '....';
 const Header = () => {
   return (
     <View className="flex-row justify-between items-center">
-      <View className="flex-grow-0 flex-row relative gap-2 ml-4 ">
+      <Stack direction="row" className="relative gap-2 ml-4 ">
         <Text className="pt-3 flex-wrap text-[28px] font-bold self-center text-[#071648]">
           {month}월
         </Text>
-        <Text className="flex-grow-0 flex-wrap self-end font-medium text-[#a8a8a8]">{year}</Text>
-      </View>
-      <Text className="flex-grow-0 items-center text-center text-sm font-bold mr-4 text-[#071648]">
+        <Text className="flex-wrap self-end font-medium text-[#a8a8a8]">{year}</Text>
+      </Stack>
+      <Text className="items-center text-center text-sm font-bold mr-4 text-[#071648]">
         인문캠퍼스
       </Text>
     </View>
