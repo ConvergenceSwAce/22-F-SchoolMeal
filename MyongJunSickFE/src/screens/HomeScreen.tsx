1f022faecb5b46deae8cf38383e74f06ec498119
김태강 <k_2684@naver.com>
2023-04-05 22:42:44 +0900

    chore: HomeScreen Props data 수정

--- MyongJunSickFE/src/screens/HomeScreen.tsx
+++ MyongJunSickFE/src/screens/HomeScreen.tsx
@@ -4,15 +4,15 @@ import {SafeAreaView} from 'react-native';
 import DayPicker from '../components/DayPicker';
 import Header from '../components/Header';
 import MealCard from '../components/MealCard';
-import dummyData from '../utils/dummyData.json';
+import {getMealData} from '../utils/MealData';
 
 const Home = () => {
   return (
     <SafeAreaView>
       <Header />
       <DayPicker />
-      <MealCard course={'A'} data={dummyData} />
-      <MealCard course={'B'} data={dummyData} />
+      <MealCard course={'A'} data={getMealData('월').lunchA} />
+      <MealCard course={'B'} data={getMealData('월').lunchB} />
     </SafeAreaView>
   );
 };
