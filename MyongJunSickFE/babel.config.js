0292c25ff483e0da3978376e786260b6adf029a9
김태강 <k_2684@naver.com>
2023-04-07 06:15:00 +0900

    fix: .env ts 설정 fix

--- MyongJunSickFE/babel.config.js
+++ MyongJunSickFE/babel.config.js
@@ -2,14 +2,16 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'nativewind/babel',
-    'module:react-native-dotenv',
-    {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true,
-    },
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
   ],
 };
