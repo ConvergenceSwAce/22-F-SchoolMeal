7d7b396c022ca2abedb97b49893b664d11fff2a6
김태강 <k_2684@naver.com>
2023-04-09 23:14:48 +0900

    style: campus 화면 별 Title switch

--- MyongJunSickFE/src/components/Header.tsx
+++ MyongJunSickFE/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import {Stack, Text, View} from 'native-base';
 import React from 'react';
 import {nowMonth, nowYear} from '../utils/Day';
+import {useSelector} from 'react-redux';
+import {campus} from '../redux/slices/setting';
 
 const month: string = nowMonth() || '..';
 const year: string = nowYear() || '....';
 
 const Header = () => {
+  const getCampus: boolean = useSelector(campus);
+
   return (
     <View className="w-screen flex-row justify-between px-[16px]">
       <Stack className="flex-row gap-2">
@@ -15,7 +19,7 @@ const Header = () => {
         <Text className="self-end font-medium text-[#a8a8a8]">{year}</Text>
       </Stack>
       <Text className="pt-3 items-center text-center text-sm font-bold text-[#071648]">
-        인문캠퍼스
+        {getCampus ? '자연캠퍼스' : '인문캠퍼스'}
       </Text>
     </View>
   );
