c8fc356fc58d04b2cd1f02011134c8c4697a2583
김태강 <k_2684@naver.com>
2023-04-13 14:35:29 +0900

    chore: error module 추가

--- MyongJunSickFE/src/screens/HomeScreen.tsx
+++ MyongJunSickFE/src/screens/HomeScreen.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 
 import DayPicker from '../components/DayPicker';
 import Header from '../components/Header';
-import {useGetIncamMealQuery} from '../redux/api/mealDataApi';
+import {useGetIncamMealQuery, useGetJacamMealQuery} from '../redux/api/mealDataApi';
 import ErrorModal from '../components/ErrorModal';
 import IncamPage from '../pages/IncamPage';
 import JacamPage from '../pages/JacamPage';
 import {useSelector} from 'react-redux';
 import {campus} from '../redux/slices/setting';
-// import JacamPage from '../pages/JacamPage';
 
 const Home = () => {
   const getCampus: boolean = useSelector(campus);
 
   const {isError} = useGetIncamMealQuery({
-    pollingInterval: 3000,
     refetchOnReconnect: true,
     refetchOnMountOrArgChange: true,
     skip: false,
   });
 
-  if (isError) {
+  const {isError: isError2} = useGetJacamMealQuery({
+    refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true,
+    skip: false,
+  });
+
+  if (isError || isError2) {
     return <ErrorModal />;
   }
 
